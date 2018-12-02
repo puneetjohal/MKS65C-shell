e890f33d9341a1d5206f54c3c2d00c3c7d0fff7f
Puneet Johal <pjohal@stuy.edu>
2018-12-02 17:31:25 -0500

    working on exec

--- shell.c
+++ shell.c
@@ -23,29 +23,30 @@ int countTokens (char * line){
 //reads through the line, separating the command from its arguments
 char** parse_args(char * line){
   int tokens = countTokens(line);
-  char ** args = malloc(sizeof(char *) * (tokens+1));
-  int i = 0;
-  for (i; i < tokens; i++){
+  char ** args = malloc(sizeof(char *) * (tokens));
+  for (int i = 0; i < tokens; i++){
     args[i] = strsep(&line, " ");
   }
-  //args[i+1] = NULL;
   return args;
 }
 
 //forks and execs command from line
 void run_command(){
-  char buf[100] = "ls -l -a";
-  printf("$ ");
-  fgets(buf, 100, stdin);
-  printf("\n");
-  printf("%s\n", buf);
-  char ** args = parse_args(buf);
-  execvp(args[0], args);
-  free(args);
+
 }
 
 int main(){
-  run_command();
+  printf("$ ");
+  char buf[100];
+  fgets(buf, 100, stdin);
+  buf[strlen(buf)-1] = 0;
+  printf("\n");
+  printf("%s\n", buf);
+  int tokens = countTokens(buf);
+  char ** args = malloc(sizeof(char *) * (tokens));
+  args = parse_args(buf);
+  execvp(args[0], args);
+  free(args);
   return 0;
 }
 
